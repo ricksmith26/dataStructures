65b47791ca4d8db34d3eb04bf52ce009f9f2cfe2
ricksmith26 <ricksmith26@hotmail.com>
2018-04-25 18:03:25 +0100

    complete and tested x10

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,8 +1,80 @@
-
-function createStack () {
-
-  // build your stack object and its methods inside this factory function
-
+function addToStack(item) {
+  this.storage[++this.quantity] = item;
 }
 
-module.exports = createStack;
+function popFromStack() {
+  delete this.storage[this.quantity];
+  --this.quantity;
+}
+
+function dequeFromStack() {
+  delete this.storage[this.frontCount];
+  --this.quantity;
+  ++this.frontCount;
+}
+
+function isEmpty() {
+  if (this.quantity === 0) {
+    return true;
+  } else return false;
+}
+
+function getSize() {
+  return this.quantity;
+}
+
+function uniqPush(item) {
+  if (!this.storage.includes(item)) this.storage[++this.quantity] = item;
+}
+
+function createStack() {
+  // build your stack object and its methods inside this factory function
+  let setStack = {};
+  setStack.quantity = 0;
+  setStack.frontCount = 1;
+  setStack.storage = {};
+  setStack.addToStack = addToStack;
+  setStack.popFromStack = popFromStack;
+  setStack.dequeFromStack = dequeFromStack;
+  setStack.isEmpty = isEmpty;
+  setStack.getSize = getSize;
+  return setStack;
+}
+
+const myStack = createStack();
+
+myStack.addToStack("funcCreateStack");
+
+module.exports = {
+  createStack,
+  addToStack,
+  popFromStack,
+  dequeFromStack,
+  isEmpty,
+  getSize,
+  uniqPush
+};
+
+/*function addToBasket(item1, item2) {
+  this.basket[++this.quantity] = item1;
+  this.basket[++this.quantity] = item2;
+}
+
+function createAccount(name, country, accountNo) {
+  const newAccount = {};
+  newAccount.name = name;
+  newAccount.country = country;
+  newAccount.accountNo = accountNo;
+  newAccount.basket = {};
+  newAccount.quantity = 0;
+  newAccount.addToBasket = addToBasket;
+  return newAccount;
+}
+
+const mitchAccount = createAccount('mitch', 'UK', '123123');
+const JDAccount = createAccount('JD', 'UK', '424242');
+
+// IMPLICIT BINDING
+// JDAccount.addToBasket('guitar');
+// mitchAccount.addToBasket('piano');
+// console.log(mitchAccount.basket);*/
--- spec/stack.spec.js
+++ spec/stack.spec.js
@@ -1,8 +1,105 @@
+const {
+  createStack,
+  addToStack,
+  popFromStack,
+  dequeFromStack,
+  isEmpty,
+  getSize,
+  uniqPush
+} = require("../data-structures/stack.js");
+const { expect } = require("chai");
 
-const createStack = require('../data-structures/stack.js');
-
-describe('createStack',() => {
-  it('',() => {
-    
+describe("createStack", () => {
+  it("creates an instance with a quantity property set to 0", () => {
+    const inputStack = createStack();
+    const actual = inputStack.quantity;
+    const expected = 0;
+    expect(actual).to.equal(expected);
+  });
+  it("creates an instance with a storage property set to an empty object", () => {
+    const inputStack = createStack();
+    const actual = inputStack.storage;
+    const expected = {};
+    expect(actual).to.eql(expected);
+  });
+  it("returns the storage with the added element", () => {
+    const inputStack = createStack();
+    inputStack.addToStack("funcCreateStack");
+    const actual = inputStack.storage;
+    const expected = { 1: "funcCreateStack" };
+    expect(actual).to.eql(expected);
+  });
+  it("returns the storage with the added element", () => {
+    const inputStack = createStack();
+    inputStack.addToStack("hello");
+    inputStack.addToStack("next item");
+    inputStack.addToStack("last item");
+    const actual = inputStack.storage;
+    const expected = { 1: "hello", 2: "next item", 3: "last item" };
+    expect(actual).to.eql(expected);
   });
 });
+describe("Queue", () => {
+  it("popFromStack: removes the last element from an array and returns the object", () => {
+    const inputStack = createStack();
+    inputStack.addToStack("funcCreateStack");
+    inputStack.addToStack("2ndfuncCreateStack");
+    inputStack.popFromStack();
+    const actual = inputStack.storage;
+    const expected = { "1": "funcCreateStack" };
+    expect(actual).to.eql(expected);
+  });
+  it("dequeFromStack: removes the first element from an array and returns the object", () => {
+    const inputStack = createStack();
+    inputStack.addToStack("funcCreateStack");
+    inputStack.addToStack("2ndfuncCreateStack");
+    inputStack.dequeFromStack();
+    const actual = inputStack.storage;
+    const expected = { "2": "2ndfuncCreateStack" };
+    expect(actual).to.eql(expected);
+  });
+  it("dequeFromStack: removes the first element from an array and returns the object", () => {
+    const inputStack = createStack();
+    inputStack.addToStack("funcCreateStack");
+    inputStack.addToStack("2ndfuncCreateStack");
+    inputStack.addToStack("final");
+    inputStack.dequeFromStack();
+    inputStack.dequeFromStack();
+    const actual = inputStack.storage;
+    const expected = { "3": "final" };
+    expect(actual).to.eql(expected);
+  });
+  it("isEmpty: returns true if no more items can be dequeued and there is no front item", () => {
+    const inputStack = createStack();
+    inputStack.isEmpty();
+    const actual = inputStack.isEmpty();
+    expect(actual).to.be.true;
+  });
+  it("isEmpty returns false if no more items can be dequeued and there is no front item", () => {
+    const inputStack = createStack();
+    inputStack.addToStack("funcCreateStack");
+    inputStack.addToStack("funcCreateStack");
+    inputStack.isEmpty();
+    const actual = inputStack.isEmpty();
+    expect(actual).to.be.false;
+  });
+  it("getSize: returns the total items in the queue", () => {
+    const inputStack = createStack();
+    inputStack.addToStack("funcCreateStack");
+    inputStack.addToStack("2ndfuncCreateStack");
+    inputStack.getSize();
+    const actual = inputStack.quantity;
+    const expected = 2;
+    expect(actual).to.eql(expected);
+  });
+});
+/*xdescribe("Sets", () => {
+  it("popFromStack: removes the last element from an array and returns the object", () => {
+    const inputStack = createStack();
+    inputStack.addToStack("funcCreateStack");
+    inputStack.addToStack("2ndfuncCreateStack");
+    inputStack.popFromStack();
+    const actual = inputStack.storage;
+    const expected = { "1": "funcCreateStack" };
+    expect(actual).to.eql(expected);
+  });*/
